fb83987762b42e136fedbd69d05c8cdced8b814f
SergioFerreira <sergio.ferreira@ctw.bmwgroup.com>
2021-01-26 17:18:55 +0000

    implement pinpad

--- lib/ui_bits.dart
+++ lib/ui_bits.dart
@@ -1,5 +1,7 @@
 library ui_bits;
 
+export 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 export 'src/animations/animations.dart';
 export 'src/components/components.dart';
 export 'src/themes/theme_factory.dart';
---
+++ sample_app/lib/pin_pad_page_sample.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/widgets.dart';
+import 'package:ui_bits/ui_bits.dart';
+
+class PinPadPageSample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var pinField = Field.asText();
+    return Container(
+      child: Column(
+        children: [
+          BitInputPasswordField(
+            const FieldLabels(
+              label: 'Pin',
+              icon: FontAwesomeIcons.solidUserCircle,
+            ),
+            field: pinField,
+          ),
+          SizedBox(height: context.sizes.extraLarge),
+          BitPinPad(
+            pinField: pinField,
+          ),
+        ],
+      ),
+    );
+  }
+}
